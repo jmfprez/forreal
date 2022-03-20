b8f0a435db616b5302363dc25e55ce6bf6f1f59b
DankestCrypto <cryptogriips@protonmail.com>
2022-03-19 21:51:30 -0700

    commit on 'master'

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,2 @@
 <h1>Welcome to SvelteKit yooooo</h1>
-<p>Visit fgtjsrfgjhz<a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Vis<a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
